4280b6c111ec44cd777fb36d1f4e58640faaece5
Deenie-Wallace <jubilant.deenie@gmail.com>
2018-07-02 22:22:42 -0700

    grader review updates

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -1,13 +1,13 @@
 
 // icon vars
-let grinder="https://cdn0.iconfinder.com/data/icons/coffee-brewing-glyph/16/7-512.png";
-let chemex="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_chemex-512.png";
-let plant="https://cdn1.iconfinder.com/data/icons/barista/256/barista-icons_coffea-arabica-512.png";
-let house="https://cdn1.iconfinder.com/data/icons/barista/256/barista-icons_coffee-shop-sign-512.png";
-let cup="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_coffee-to-go-512.png";
-let espresso="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_espresso-machine-with-cup-512.png";
-let perk="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_espresso-maker-512.png";
-let press="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_french-press-512.png";
+const grinder="https://cdn0.iconfinder.com/data/icons/coffee-brewing-glyph/16/7-512.png";
+const chemex="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_chemex-512.png";
+const plant="https://cdn1.iconfinder.com/data/icons/barista/256/barista-icons_coffea-arabica-512.png";
+const house="https://cdn1.iconfinder.com/data/icons/barista/256/barista-icons_coffee-shop-sign-512.png";
+const cup="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_coffee-to-go-512.png";
+const espresso="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_espresso-machine-with-cup-512.png";
+const perk="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_espresso-maker-512.png";
+const press="https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_french-press-512.png";
 
 
 // shuffle vars
@@ -36,115 +36,104 @@ let time;
 
 
 // scoreboard vars
-let starRate = document.getElementsByClassName('fa-star');
+const starRate = document.getElementsByClassName('fa-star');
 let clearCards;
 let game;
 
 
  // start game
-play(cards, deal); 
- 
+play(cards, deal);
+
 // timer starts when first card is clicked
 for (i = 0; i < cards.length; i++) {
-		
 	let card = cards[i];
-			
+	
 	card.addEventListener ('click', function(event) {
-					
-	if(!game){
-		start = performance.now();
-			
-		game = true;
-	
-		displayTime = setInterval(startDisplay, 1000);
-	}
-		
-	clickCount += 1; 		
-				
-	this.classList.add('flip-2-hor-bottom-1');	
-	this.childNodes[1].style.visibility = 'visible';
-							
-		
-	if (clickCount == 1) {
-		
-		cardOne = this;
-		
-		pickOne = cardOne.childNodes[1].src;
-		
-		cardOne.classList.add('open');	
-		
-		this.style.pointerEvents = "none";			
-	}
 
-// display match count		
-	if (clickCount == 2 && this != cardOne) {     
-			
-		cardTwo = this;
-			
-		let pickTwo = cardTwo.childNodes[1].src;  
-			
-		cardTwo.classList.add('open');
-							
-		if(pickOne == pickTwo){
-						
-			cardTwo.classList.add('match');
-							
-			cardOne.classList.add('match');
-							
-			matchCount += 1;
-		
-			document.getElementsByClassName('matches')[0].textContent= (matchCount);
-			
-			cardOne.style.pointerEvents = "auto";
-			
-			clickCount = 0;
-	
+		if(!game){
+			start = performance.now();
+			game = true;
+			displayTime = setInterval(startDisplay, 1000);
 		}
-			
- //winning conditions			
-		if (matchCount == 8) {
-						
-			game = false;
-			
-			stopwatch[1].textContent = ('Time ' + time + ' seconds');
-				
- //display popup modal 
-			document.getElementById('myModal').style.display = 'block';	
-			
+
+		clickCount += 1;
+
+		this.classList.add('flip-2-hor-bottom-1');
+		this.childNodes[1].style.visibility = 'visible';
+
+		if (clickCount == 1) {
+			cardOne = this;
+			pickOne = cardOne.childNodes[1].src;
+			cardOne.classList.add('open');
+			this.style.pointerEvents = "none";	
+		}
+
+
+	// display match count
+		if (clickCount == 2 && this != cardOne) {
+
+			for (i = 0; i < 16; i++) {
+				cards[i].style.pointerEvents = "none";
+			}
+
+			cardTwo = this;
+
+			let pickTwo = cardTwo.childNodes[1].src;
+
+			cardTwo.classList.add('open');
+
+			if(pickOne == pickTwo){
+				cardTwo.classList.add('match');
+				cardOne.classList.add('match');
+
+				matchCount += 1;
+
+				document.getElementsByClassName('matches')[0].textContent= (matchCount);
+				cardOne.style.pointerEvents = "auto";
+				clickCount = 0;
+			}
+
+	 //winning conditions			
+			if (matchCount == 1) {					
+				game = false;
+				stopwatch[1].textContent = ('Time ' + time + ' seconds');
+
+	 //display popup modal
+			document.getElementById('myModal').style.display = 'block';
+
 			starRate[3].classList.add('rotate-scale-up');
 			starRate[4].classList.add('rotate-scale-up');
 			starRate[5].classList.add('rotate-scale-up');
+
+			resetDeck();
 			
-			resetDeck();				
-		} 	else {
-				
-// no match, display miss, and reset cards				
-			window.setTimeout(resetCards, 1000);
-											
+			} else {
+			// no match, display miss, and reset cards		
+			window.setTimeout(resetCards, 800);						
+			}
+			
+			clickCount = 0;
+			
+			} else {
+			clickCount = 1;
 		}
-		clickCount = 0;		
-	} 	else {
-		clickCount = 1;
-	}
-});
+	});
 }
 
 // click 'fa-repeat' to replay resets the board
 let restart = document.getElementsByClassName('fa-repeat');
 
-for(i = 0; i < 2; i++){		
+for(i = 0; i < 2; i++){
 	restart[i].addEventListener('click', function(event){
 		resetDeck();
-		
+		resetCards();
 		modal.style.display = "none";
-		
-		clearInterval(displayTime);	
-		
+		clearInterval(displayTime);
 		resetStars();
 	});
 }
 
- // shuffle function from http://stackoverflow.com/a/2450976 
+ // shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -158,11 +147,11 @@ function shuffle(array) {
 
     return array;
 }
-	
+
 // reset stars, deck, and clear score-board
 
-function resetStars() {	
-	for (i = 0; i < 6; i++) {	
+function resetStars() {
+	for (i = 0; i < 6; i++) {
 			starRate[i].style.visibility = 'visible';
 		}
 }
@@ -173,45 +162,38 @@ function resetDeck(){
 		cardTwo.childNodes[1].style.visibility = 'hidden';
 		cardOne.childNodes[1].style.visibility = 'hidden';
 	}
-	
+
 	document.getElementsByClassName('matches')[0].textContent= ('');
 	document.getElementsByClassName('moves')[0].textContent= ('');
 	document.getElementsByClassName('timer')[0].textContent= ('Time:');
-	
+
 	matchCount = 0;
 	turnCount = 0;
-	clickCount = 0;	
-	
+	clickCount = 0;
+
 	clearInterval(displayTime);
-	
+
 	deal = shuffle(deck);
 	play(cards, deal);
-	
+
 }
 
 // add card icons
 function play(cards, deal){
 
 	for (i = 0;i < 16; i++){
-
 		let shuffle = cards[i].childNodes[1];
-	
 		shuffle.src = deal[i];
-		
-		shuffle.style.visibility = 'hidden';
-				
+		shuffle.style.visibility = 'hidden';			
 	}
 }
 
 // timer 
 function startDisplay() {
-			
-	currentTime = performance.now();	
-		
-	let totalSeconds = ((currentTime - start)/1000).toFixed(0);
-	
-	let minutes = (totalSeconds/60).toFixed(0);
+	currentTime = performance.now();
 
+	let totalSeconds = ((currentTime - start)/1000).toFixed(0);
+	let minutes = (totalSeconds/60).toFixed(0);
 	let seconds = totalSeconds % 60;
 
 	if (seconds < 10){
@@ -220,12 +202,12 @@ function startDisplay() {
 	else{
 		time = minutes + ":" + seconds;
 	}
-		
+
 	stopwatch[0].textContent = ('Time ' + time);
 }
 
 function resetCards(){
-					
+
 	cardOne.classList.add('flip-2-hor-bottom-2');
 	cardTwo.classList.add('flip-2-hor-bottom-2');
 
@@ -237,33 +219,32 @@ function resetCards(){
 
 	window.setTimeout(removeAnimation, 300);
 
-	cardOne.style.pointerEvents = "auto";
-	
+
+	for (i = 0; i < 16; i++) {
+			cards[i].style.pointerEvents = "auto";
+		}
+
 	turnCount += 1;
-						
+
 	document.getElementsByClassName('moves')[1].textContent= (turnCount);
 	document.getElementsByClassName('moves')[0].textContent= (turnCount);
-
-// misses change star rating					
+	
+// misses change star rating
 
 	if (turnCount == 5) {
-		
 		starRate[2].style.visibility = 'hidden';	
-		starRate[5].style.visibility = 'hidden';	
+		starRate[5].style.visibility = 'hidden';
 	}
 	if (turnCount == 10) {
-		
 		starRate[1].style.visibility = 'hidden'; 
-		starRate[3].style.visibility = 'hidden'; 
-	}							
-}			
+		starRate[3].style.visibility = 'hidden';
+	}
+}
 
 
 function removeAnimation() {
-	
 	cardOne.classList.remove('flip-2-hor-bottom-2','flip-2-hor-bottom-1');
 	cardTwo.classList.remove('flip-2-hor-bottom-2', 'flip-2-hor-bottom-1');
-
 }
 
 
@@ -275,16 +256,22 @@ const span = document.getElementsByClassName("close")[0];
 
 // when the user clicks on <span> (x), close modal
 span.onclick = function() {
-    resetStars();
+	resetStars();
 	modal.style.display = "none";
+	
+	for (i = 0; i < 16; i++) {
+			cards[i].style.pointerEvents = "auto";
+		}
 }
 
 // when the user clicks anywhere outside of modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        resetStars();
+		resetStars();
 		modal.style.display = "none";
+		
+		for (i = 0; i < 16; i++) {
+			cards[i].style.pointerEvents = "auto";
+		}
     }
 }
-
-
