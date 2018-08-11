acc70006210d07d498019aa62c14cba9cb2c6450
Deenie-Wallace <jubilant.deenie@gmail.com>
2018-08-10 19:36:28 -0700

    inital cat commit

---
+++ cat-clicker/app.js
@@ -0,0 +1,11 @@
+const elem = document.getElementById('image');
+count = 0;
+elem.addEventListener('click', function(){
+  //the element has been clicked... do stuff here
+
+  console.log('clicked');
+  count += 1;
+  
+  let clicks = document.getElementById('clicks');
+   clicks.innerHTML = 'Clicks:' + count; 
+}, false);
