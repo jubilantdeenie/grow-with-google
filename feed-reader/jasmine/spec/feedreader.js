2116633ee96491368c0f087bdcd49acd6af34f0d
Deenie-Wallace <jubilant.deenie@gmail.com>
2018-08-16 22:33:01 -0700

    resubmission update with jasmine errors

--- feed-reader/jasmine/spec/feedreader.js
+++ feed-reader/jasmine/spec/feedreader.js
@@ -1,4 +1,3 @@
-
 $(function() {
    
     describe('RSS Feeds', function() {
@@ -24,115 +23,61 @@ $(function() {
 
     /* New test suite "The menu" */
    describe("The menu", function() {
-        let body = $('body');
-
         /* This test ensures the menu element is
          * hidden by default.*/
 
-         it('is hidden by default', function() {
+        it('is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
             });
 
-        //double check this is working right
-         it('is visible when the icon is clicked', function() {
+        it('is visible when the icon is clicked', function() {
                 // First click of menu 
                 $('.menu-icon-link').click();
                 expect($('body').hasClass('menu-hidden')).toBe(false);
                 // Second click of menu
                 $('.menu-icon-link').click();
                 expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
+        });
     });
-                               /*// let clicked = $('click');
-                                let menu = $('icon-list');
-                                let link = $('.menu-icon-link');
-
-                                let callback = jasmine.createSpy('menu','.menu-icon-link');
-
-                                if(link.click(callback)) {
-                                    expect(body.hasClass('menu-hidden')).toBe(true);
-                                }
-
-                                // $(selector).click()
-                                //let callback = jasmine.createSpy('menu','.menu-icon-link');
-                  */
 
     /* New test suite "Initial Entries" */
     describe('Initial Entries', function() {
             
         /* When the loadFeed function is called and completes its work, there is at least
          * a single .entry element within the .feed container.*/
-         beforeEach(function(done) {
+        beforeEach(function(done) {
             loadFeed(0, function () {
                 done();
             });
-          });
+        });
 
-         it('has at least one entry', function() {
+        it('has at least one entry', function() {
             let entries = $('.feed .entry');
             expect(entries.length).toBeGreaterThan(0); 
-            });
         });
+        //});
     });
 
     /* New test suite "New Feed Selection" */
     describe('New Feed Selection', function() {
 
         beforeEach(function(done){
-            loadFeed(1, function(){
-
-                feedOne = $('.feed').html();   
-           
             loadFeed(0, function(){
-                feedTwo = $('.feed').html();
+                let feedOne = $('.feed').html();   
+           });
+
+            loadFeed(1, function(){
+                let feedTwo = $('.feed').html();
                 done();
             });
         });
-     });
+     //});
 
         it('changes content', function() {
             expect(feedOne).not.toBe(feedTwo);
         });
-      //});
-  // });
-}());
-
- /*           const feed = $('.feed');
-            const feedOne = $('.feed').html();
-            //feedOne = feed.html();
-
-            
-        /* This test ensures when a new feed is loaded
-         * by the loadFeed function the content changes.
-         */
-
-  /*          beforeEach(function(done) {
-            loadFeed(0);
-            Array.from(feed.children).forEach(function(entry) {
-                feedOne.push(entry.innerText);
-                });
-            loadFeed(1, done); 
-            }); 
-*/
-
-
-
-            /* beforeEach(function(done){
-        loadFeed(0, function(){
-        // set the value of feedOne here
-        loadFeed(1, done); // This is inside the first loadFeed's callback because we only want to fetch the next set of feeds when the first one is done loading.
     });
-});*/
-
-
-
-
-       
-      //     it('changes content', function() {
-      //        Array.from(feed.children).forEach(function(entry,index) {
-    //        expect(entry.innerText === feedOne[index]).toBe(false);
-      //      });
-    // });
+}());
 
 
 /*
@@ -146,6 +91,8 @@ $(function() {
    link: https://matthewcranford.com/feed-reader-walkthrough-part-3-menu-test-suite/
    link: https://matthewcranford.com/feed-reader-walkthrough-part-4-async-tests/
 
+   Alain Cadenat
+   link: https://www.youtube.com/watch?v=pPt4oOKNdEk
 
  'toBe',
       'toBeCloseTo',
