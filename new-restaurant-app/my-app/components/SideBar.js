7bb114020d3b61bc6a768edb03ad942ed7a417c6
Deenie-Wallace <jubilant.deenie@gmail.com>
2018-09-29 17:51:37 -0700

    sidebar no bugs not displaying

--- new-restaurant-app/my-app/components/SideBar.js
+++ new-restaurant-app/my-app/components/SideBar.js
@@ -3,6 +3,7 @@ import { withStyles } from 'material-ui/core/styles';
 import Button from 'material-ui/core/Button';
 
 import * as utils from '../utils'
+import MapDataAPI from './MapDataAPI.js'
 //import RenderToLayer from 'material-ui/internal/RenderToLayer';
 //import { FloatingActionButton } from 'material-ui';
 
@@ -17,7 +18,13 @@ const styles = theme => ({
         width: theme.spacing.unit * 50,
         backgroundColor: theme.palette.background.paper,
         maxWidth: '95%'
-    }
+    },
+    card: {
+        maxWidth: 330,
+    },
+    media: {
+        objectFit: 'cover'
+    },
 });
 
 class SideBar extends Component {
@@ -28,75 +35,56 @@ class SideBar extends Component {
     }
 
     componentWillMount() {
-        // add setState for photo_list
+        utils.getFourSquarePhotos('gallery').then(photos_list => {
+            this.setState({photos_list });
+        });
 }
 
-render() {
-    let displaySidebar = this.props.sidebarOpen ? "block" : "none";
-    let { classes } = this.props;
+        render() {
+            let displaySidebar = this.props.sidebarOpen ? "block" : "none";
+            let { classes } = this.props;
+        
+            return (
+                <section id="sidebar" style={{ display: displaySidebar  }}>
+                    <div id="sidebar-inner">
 
-    return (
-        <section id="sidebar" style={{ display: displaySidebar  }}>
-            <div id="sidebar-inner">
-                { this.props.foursquareData && 
-                <p className="text-center">
-                    <Button variant="contained" color="primary" className={classes.button} onClick={this.props.handleShow}>
-                     Info
-                    </Button>
-                </p>    
-                }
+        <p className="text-center">
+        <Button variant="contained" color="primary" className={classes.button}
+            onClick={() => {this.setState(state => ({ showingVenues : !state.showingVenues})) }}>
+            { this.state.showingVenues ? 'Hide' : 'Show'} Venues
+        </Button>
+        </p>
 
-                <p className="text-center">
-                <Button variant="contained" color="primary" className={classes.button}
-                    onClick{() => {this.setState(state => ({ showingVenues : !state.showingVenues})) }}>
-                    { this.state.showingVenues ? 'Hide' : 'Show'} Venues
-                </Button>
-                </p>
-                
-                
-                <p className="text-center">
-                    <Button variant="contained" color="primary" className={classes.button}>
-                    on-click={() => { this.setState( state => ({ showingPictures : !showingPictures })) }}>
-                    { this.state.showingPictures ? 'Hide' : 'Show'} Photos
-                    </Button>
-                </p>
+        <p className="text-center">
+        <Button variant="contained" color="primary" className={classes.button}>
+        on-click={() => { this.setState( state => ({ showingPictures : !showingPictures })) }}>
+        { this.state.showingPictures ? 'Hide' : 'Show'} Photos
+        </Button>
+        </p>
 
-            {
-                this.state.showingVenues && 
-                <div>
-                    <input className="transition middlr input-s1" placeholder="Filter Venues"
-                        value={this.props.query} onChange={(e) => {this.props.filterVenues(e.target.value) }} />
-                    <ul id="places-list"></ul>
-            {
-                this.props.filtered && this.props.filtered.map((venue, key) => (
-                    <li tabIndex="0" className="transition" title={ venue.name }
-                    <h5><strong>
-                    <span className="venue-name"
-                     onClick={() => {window.open(/*add venue web*/)}}
-                     onKeyPress={(even) => { this.linkspanKeyEnter(event, venue) }}>
-                        { venue.name }
-                    </span>
-                    </strong></h5>
-                    </p>  
-                     {
-                        venue.location.formattedAddress.map((value.index) => {
-                            return index === (venue.location.formattedAddress.length -1) ?
-                            <span key={index}><span>{value}</span><br/></span>)
-                        })
-                    }
-                    </p>
-                    <p>{ venue.hereNow.summary</p>
-                    <img className="venuePhoto" src={/* insert*/ } alt= {venue.name }
-                </li>
-                })
-            }
-        </ul>
-    </div>
+        {
+            this.state.showingVenues && 
+            <div>
+                <input className="transition middlr input-s1" placeholder="Filter Venues"
+                    value={this.props.query} onChange={(e) => {this.props.filterVenues(e.target.value) }} />
+                <ul id="places-list"></ul>
+        
+
+    <br/>
+
+    <div ClassName="">
+        {
+        this.props.filtered && this.props.filtered.map((venue, key) => (
+            <VenueCard key={key} venue={venue} li_clikc={this.props.li_click} liKeyEnter={this.props.liKeyEnter}/>
+        ))
+    }
+</div>
+</div>
 }
 
 <br/>
 
-    </div>
+</div>
 </section>
 );
 }
@@ -104,13 +92,13 @@ render() {
 
 export default withStyles(styles)(SideBar);
 
-  
-   { /*let displaySidebar = this.props.sidebarOpen ? "block" : "none";
-    let { classes } = this.props;
 
-    return(
-        <section id="sidebar" style={{ display: displaySidebary }}>
-            <div id="sidebar-inner">
-             </div>
-        </section>
-    ); */}
+{ /*let displaySidebar = this.props.sidebarOpen ? "block" : "none";
+let { classes } = this.props;
+
+return(
+<section id="sidebar" style={{ display: displaySidebary }}>
+    <div id="sidebar-inner">
+        </div>
+</section>
+); */}
